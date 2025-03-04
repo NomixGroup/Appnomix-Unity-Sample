6606496a8bc6dc8b226bebeb9d73c4b095770d7c
narcisbadea1 <narcisbadea@gmail.com>
2025-03-04 13:48:54 +0200

    fixes

--- unity-6000/Appnomix-Unity/Assets/AppnomixKeyboardSDK/Editor/SetupXcode.sh
+++ unity-6000/Appnomix-Unity/Assets/AppnomixKeyboardSDK/Editor/SetupXcode.sh
@@ -75,13 +75,9 @@ cp -R "$TEMP_DIR/Appnomix Keyboard Resources/Appnomix Keyboard/"/* "$APP_EXTENSI
 mkdir -p "$PROJECT_PATH/Frameworks"
 cp -R "$TEMP_DIR/Appnomix Keyboard Resources/Appnomix Frameworks/"/* "$PROJECT_PATH/Frameworks"
 
-mkdir -p "$PROJECT_PATH/Appnomix.xcassets"
-cp -R "$TEMP_DIR/Appnomix Keyboard Resources/MainApp/Appnomix.xcassets/"/* "$PROJECT_PATH/Appnomix.xcassets"
-
-cp -R "$TEMP_DIR/Appnomix Keyboard Resources/Appnomix Frameworks/"/* "$PROJECT_PATH"
 mkdir -p "$PROJECT_PATH/MainApp/Appnomix.xcassets"
-
 cp -R "$TEMP_DIR/Appnomix Keyboard Resources/MainApp/Appnomix.xcassets/"/* "$PROJECT_PATH/MainApp/Appnomix.xcassets"
+
 cp -R "$TEMP_DIR/Appnomix Keyboard Resources/MainApp/"/* "$PROJECT_PATH/MainApp"
 
 cd "$PROJECT_PATH"
